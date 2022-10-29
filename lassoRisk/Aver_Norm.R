a84bfa88becde6bdb830409459f0da29127977b5
peixianzheng <116952887+peixianzheng@users.noreply.github.com>
2022-10-29 23:53:04 +0800

    lassoRisk
    lassoRisk codes

---
+++ lassoRisk/Aver_Norm.R
@@ -0,0 +1,14 @@
+setwd("~/desktop/1")
+tt=read.table( "symbol.txt",header = T,row.names =1)
+
+  
+
+ttMean=apply(tt,1,mean)
+
+ttAVer=sweep(tt, 1, ttMean, '-')  ##每列减去其平均值
+
+gene=rownames(ttAVer)
+ttRes=cbind(gene,ttAVer)
+write.table( ttRes, file="symbol_Aver.txt",sep="\t",row.names=F ,quote=F)
+
+
